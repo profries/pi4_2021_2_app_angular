ff04242bd37d05b733e334c28c6d2d02242389ec
Luis Henrique Ries <prof.ries@gmail.com>
2021-10-15 08:46:48 -0300

    Setup do projeto, criacao de componente e diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TabelaProdutosComponent } from './tabela-produtos/tabela-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaProdutosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tabela-produtos/tabela-produtos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-produtos',
+  templateUrl: './tabela-produtos.component.html',
+  styleUrls: ['./tabela-produtos.component.css']
+})
+export class TabelaProdutosComponent implements OnInit {
+  produtos = [
+    {id:1, nome:"Prod1", preco:10},
+    {id:2, nome:"Prod2", preco:20},
+    {id:3, nome:"Prod3", preco:30},
+    {id:4, nome:"Prod4", preco:40},
+    {id:5, nome:"Prod5", preco:50},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
